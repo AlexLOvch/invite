7942f3fa8d510106db6d03019a277cf82e3c23c9
Alex Ovchinnikov <alex.l.ovchinnikov@gmail.com>
2013-07-29 20:49:32 +0300

    solt to salt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-   attr_accessible :email, :encrypted_password, :solt, :name, :phone, :addr, :password, :password_confirmation
+   attr_accessible :email, :encrypted_password, :salt, :name, :phone, :addr, :password, :password_confirmation
    attr_accessor :password, :password_confirmation
 
    before_save :encrypt_password  
@@ -16,7 +16,7 @@ class User < ActiveRecord::Base
 
    def self.authenticate(email, password)  
         user = find_by_email(email)  
-        if user && user.encrypted_password == BCrypt::Engine.hash_secret(password, user.solt)  
+        if user && user.encrypted_password == BCrypt::Engine.hash_secret(password, user.salt)  
           user  
         else  
           nil  
@@ -28,8 +28,8 @@ class User < ActiveRecord::Base
 
    def encrypt_password  
     if password.present?  
-      self.solt = BCrypt::Engine.generate_salt  
-      self.encrypted_password = BCrypt::Engine.hash_secret(password, solt)  
+      self.salt = BCrypt::Engine.generate_salt  
+      self.encrypted_password = BCrypt::Engine.hash_secret(password, salt)  
     end  
    end  
 
