f477a594acb1b931cfcf3d2d2c44a441de926b05
Alex Ovchinnikov <alex.l.ovchinnikov@gmail.com>
2013-07-29 10:08:26 +0300

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Invite::Application.config.secret_token = 'eac3393e23ec7c4224a33d3b42fafa3f5ffdb7f818f3c726d05d60307fd231682134a2503ccea634ed10453e366c9bdfcdf73657143e0c831711226ef6d0865b'
