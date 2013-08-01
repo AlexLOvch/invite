275911b0c16d91f4b8a4531a9bbdbe708dfb618d
Alex Ovchinnikov <alex.l.ovchinnikov@gmail.com>
2013-08-01 21:59:28 +0300

    fix send mail settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Invite::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
 
-config.action_mailer.default_url_options = { :host => 'inviteheroku.herokuapp.com' }
+config.action_mailer.default_url_options = {host: 'inviteheroku.herokuapp.com',  from: "noreply@lisa.flyrouter.net"}
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.perform_deliveries = true
 config.action_mailer.raise_delivery_errors = true
